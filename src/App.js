4a72c5ca8fed585887a39d9a83a0d781c11a22f3
Ali Amjad <p156121@nu.edu.pk>
2021-08-17 14:28:04 +0500

    firsr commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import MobileView from "./components/MobileView/MobileView";
+import { EditorState, convertToRaw } from "draft-js";
 
-function App() {
+import "./App.css";
+import TextEditor from "./components/TextEditor/TextEditor";
+
+const App = () => {
+  const [textMarkdown, setTextMarkdown] = useState("");
+  console.log(
+    "textMarkdown =>>>>>>>>>>>>>>>>>",
+    textMarkdown,
+    typeof textMarkdown
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <div className="texteditor-container">
+        <TextEditor setTextMarkdown={setTextMarkdown} />
+      </div>
+      <div className="mobile-container">
+        <MobileView textMarkdown={textMarkdown} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/TextEditor/TextEditor.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { EditorState, convertToRaw } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import draftToHtml from "draftjs-to-html";
+import "./textEditor.css";
+
+const TextEditor = ({ setTextMarkdown }) => {
+  const [state, setState] = useState({
+    editorState: EditorState.createEmpty(),
+  });
+
+  const onEditorStateChange = (editorState) => {
+    console.log("editorState =>>>>>>>>>>>>", editorState.getCurrentContent());
+    setTextMarkdown(draftToHtml(convertToRaw(editorState.getCurrentContent())));
+
+    setState({
+      editorState,
+    });
+  };
+
+  const { editorState } = state;
+
+  return (
+    <div>
+      <Editor
+        editorState={editorState}
+        wrapperClassName="demo-wrapper"
+        editorClassName="demo-editor"
+        onEditorStateChange={onEditorStateChange}
+      />
+    </div>
+  );
+};
+
+export default TextEditor;
